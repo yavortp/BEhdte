a4f0c70795bd14c25285f1d8cdba6d8a9c6f2e55
Yavor Petkov <yavorpetkov1985@gmail.com>
2025-10-15 10:01:25 +0300

    add fav icon

--- driverEvents/src/main/java/com/example/driverevents/controller/VehicleController.java
+++ driverEvents/src/main/java/com/example/driverevents/controller/VehicleController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/vehicles")
+@RequestMapping("/api/vehicles")
 @RequiredArgsConstructor
 public class VehicleController {
 
